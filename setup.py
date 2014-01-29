db74ce8f4f867131edbd641d10d04809afb4d4c0
Rodrigo Bernardo Pimentel <rbp@isnomore.net>
2014-01-30 00:44:27 +0100

    Adding setup.py for packaing and tox.ini for Python 2/3 testing

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import io
+
+setup(
+    name='configtamer',
+    author='Rodrigo Bernardo Pimentel',
+    author_email='rbp@isnomore.net',
+    version='0.1',
+    packages=['configtamer'],
+    license='Apache License, Version 2.0',
+    description="configuration file parsing under control",
+    long_description="A clean, flexible configuration file format (and parser).",
+    url='https://github.com/rbp/configtamer',
+
+    install_requires=['parsimonious>=0.5'],
+    
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        ),
+    )
